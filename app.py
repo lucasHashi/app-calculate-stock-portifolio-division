93d221dc6f18917b2b7ba35b85a4de10dd83543c
Lucas Belmonte <lucashashibelmonte@gmail.com>
2022-03-26 16:37:47 -0300

    Ajustes nas contas e download de resultado
    Troca da forma de calcular a divisao do dinheiro, agora a quantia de dinheiro que deve ir para cada ativo é igual ao total vezes a porcentagem da nota (porcentagem da nota = nota / soma das notas)
    
    Download do resultado em CSV

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from operator import index
 import pandas as pd
 import streamlit as st
 from math import gcd
@@ -12,6 +13,7 @@ def main():
 
     stocks_selected = st.multiselect('Stocks', df_stocks_sectors['ticker'])
 
+    # ----------- GRADE SLIDERS FOR EACH STOCK LISTED -----------
     if stocks_selected:
         stocks_and_grades = {}
         for stock in stocks_selected:
@@ -22,37 +24,50 @@ def main():
                 500,
                 5
             )
-    
 
 
+    # ----------- RECALCULATE BUTTON -----------
     if st.button('Recalculate division'):
         if stocks_selected:
             df_stocks_and_grades = pd.DataFrame(list(stocks_and_grades.items()), columns = ['stock','grade'])
 
-            lcm_grades = least_common_multiple_from_list(list(df_stocks_and_grades['grade']))
+            df_stocks_and_grades['percent'] = df_stocks_and_grades['grade'] / df_stocks_and_grades['grade'].sum()
 
-            df_stocks_and_grades['partitions'] = df_stocks_and_grades['grade'] / lcm_grades
+            df_stocks_and_grades['investment'] = df_stocks_and_grades['percent'] * total_invested
+            df_stocks_and_grades['investment'] = df_stocks_and_grades['investment'].round(2)
 
-            partition_from_total_invested = total_invested / df_stocks_and_grades['partitions'].sum()
+            df_stocks_and_grades_print = prepare_df_to_print(df_stocks_and_grades)
 
-            df_stocks_and_grades['investment'] = df_stocks_and_grades['partitions'] * partition_from_total_invested
+            st.dataframe(df_stocks_and_grades_print)
 
-            st.write(lcm_grades)
-            st.dataframe(df_stocks_and_grades)
+            # ----------- DOWNLOAD CSV BUTTON -----------
+            csv_stocks_and_grades = convert_to_csv(df_stocks_and_grades)
+            st.download_button(
+                label="Download as CSV",
+                data=csv_stocks_and_grades,
+                file_name='stocks_and_grades.csv',
+                mime='text/csv',
+            )
         else:
             st.write('Select one or more stocks')
 
 
+def convert_to_csv(df):
+    return df.to_csv(index=False, decimal=",").encode('utf-8')
 
-def least_common_multiple_from_list(list_values):
-    lcm = 1
-    for i in list_values:
-        lcm = lcm*i//gcd(lcm, i)
+def prepare_df_to_print(df):
+    df_print = df.copy()
+
+    df_print['percent'] = df_print['percent'].round(4) * 100
+    df_print['percent'] = df_print['percent'].apply(lambda value: "%.2f" % value + "%")
+
+    df_print['investment'] = df_print['investment'].apply(lambda value: "R$ " + "%.2f" % value)
     
-    return lcm
+    df_print.sort_values('grade', ascending=False, inplace=True)
 
+    df_print.columns = [column.capitalize() for column in df.columns]
 
-
+    return df_print
 
 
 if __name__ == '__main__':
